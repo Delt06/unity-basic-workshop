278d47a1eb368bdaed4c67d5e5e9430f408001c7
Vladislav Kantaev <vvkantaev@yandex.ru>
2021-03-05 10:44:57 +0300

    Create order demo script

---
+++ Assets/Scripts/OrderDemoScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class OrderDemoScript : MonoBehaviour
+{
+	private void Awake()
+	{
+		Debug.Log("Awake");
+	}
+
+	private void OnEnable()
+	{
+		Debug.Log("OnEnable");
+	}
+
+	private void Start()
+	{
+		Debug.Log("Start");
+	}
+
+	private void Update()
+	{
+		Debug.Log("Update");
+	}
+
+	private void OnDisable()
+	{
+		Debug.Log("OnDisable");
+	}
+
+	private void OnDestroy()
+	{
+		Debug.Log("OnDestroy");
+	}
+}
+
